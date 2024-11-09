d7c2ffb0bc8307586221adbe61a1986c0f159f43
dutra-pc <joao.vitornl@gmail.com>
2024-11-09 13:00:01 -0300

    feat: Use globalStyles

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import { StrictMode } from 'react';
+
+import GlobalStyle from './styles/globalStyles.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <GlobalStyle />
   </StrictMode>,
 )
